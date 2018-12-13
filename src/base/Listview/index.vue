3afff69d58f7f9190d3caa411ab86a1d50d4557b
tuling <tuling@funtsui.com>
2018-12-13 17:52:45 +0800

    Feat:右测快速入口实现3

--- src/base/Listview/index.vue
+++ src/base/Listview/index.vue
@@ -92,14 +92,23 @@ export default {
     },
     scrollY(newY) {
       const listHeight = this.listHeight;
+      // 当滚动到顶部,newY>0
+      if (newY > 0) {
+        this.currentIndex = 0;
+        return;
+      }
+      // 在中间部分滚动
       for (let i = 0; i < listHeight.length; i++) {
         let height1 = listHeight[i];
         let height2 = listHeight[i + 1];
-        if (!height2 || (-newY > height1 && -newY < height2)) {
+        if (-newY >= height1 && -newY < height2) {
           this.currentIndex = i;
           return;
         }
       }
+      // 当滚动到底部,且-newY大于最后一个元素的上限
+      this.currentIndex = listHeight.length - 2;
+
       this.currentIndex = 0;
     }
   }
