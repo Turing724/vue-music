54453410dfa8dde6f5f486efff812012cb657723
tuling <tuling@funtsui.com>
2018-12-26 18:35:01 +0800

    Feat：封装songList

---
+++ src/base/songList/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="songList"></div>
+</template>
--- src/components/SingerDetail/index.vue
+++ src/components/SingerDetail/index.vue
@@ -1,17 +1,30 @@
 <template>
   <transition name="slide">
-
-    <div class="singer-detail">
-      123
-    </div>
+    <music-list :songs="songs" :title="title" :bg-image="bgImage"></music-list>
   </transition>
 </template>
 <script>
 import { mapGetters } from 'vuex';
 import { ERR_OK } from 'api/config';
 import { getSingerDetail } from 'api/singer';
+import { createSong } from 'common/js/song';
+import musicList from 'components/musicList';
 export default {
+  components: {
+    musicList
+  },
+  data() {
+    return {
+      songs: []
+    };
+  },
   computed: {
+    title() {
+      return this.singer.name;
+    },
+    bgImage() {
+      return this.singer.avatar;
+    },
     ...mapGetters(['singer'])
   },
   created() {
@@ -25,25 +38,26 @@ export default {
       }
       getSingerDetail(this.singer.id).then(res => {
         if (res.code === ERR_OK) {
-          console.log(res.data);
+          this.songs = this._normalizeSongs(res.data.list);
+          console.log(this.songs);
         }
       });
+    },
+    _normalizeSongs(list) {
+      let ret = [];
+      list.forEach(item => {
+        let { musicData } = item;
+        if (musicData.songid && musicData.albummid) {
+          ret.push(createSong(musicData));
+        }
+      });
+      return ret;
     }
   }
 };
 </script>
  <style lang="stylus" scoped>
  @import '~common/stylus/variable';
-
- .singer-detail {
-   position: fixed;
-   z-index: 100;
-   top: 0;
-   left: 0;
-   right: 0;
-   bottom: 0;
-   background: $color-background;
- }
 
  .slide-enter-active, .slide-leave-active {
    transition: all 0.3s;
