5f250d0737dc2223306d293e56fb243acc6942f5
tuling <tuling@funtsui.com>
2018-12-27 17:44:32 +0800

    Feat：完成歌曲列表

--- src/components/musicList/index.vue
+++ src/components/musicList/index.vue
@@ -1,16 +1,26 @@
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <i class="icon-back"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
-      <div class="filter"></div>
+      <div class="play-wrapper" ref="playBtn">
+        <div class="play" v-show="songs.length>0">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
+      <div class="filter" ref="filter"></div>
     </div>
-    <scroll :data="songs" class="list" ref="list">
+    <div class="bg-layer" ref="layer"></div>
+    <scroll @scroll="scroll" :probe-type="probeType" :listen-scroll="listenScroll" :data="songs" class="list" ref="list">
 
       <div class="song-list-wrapper">
         <song-list :songs="songs"></song-list>
+      </div>
+      <div class="lodaing-container" v-show="!songs.length">
+        <loading></loading>
       </div>
     </scroll>
   </div>
@@ -18,10 +28,16 @@
 <script>
 import Scroll from 'base/Scroll';
 import SongList from 'base/SongList';
+import Loading from 'base/Loading';
+import { prefixStyle } from 'common/js/dom';
+const RESERVED_HEIGHT = 40;
+const transform = prefixStyle('transform');
+const backdrop = prefixStyle('backdrop-filter');
 export default {
   components: {
     Scroll,
-    SongList
+    SongList,
+    Loading
   },
   props: {
     bgImage: {
@@ -37,18 +53,63 @@ export default {
       default: ''
     }
   },
+
   computed: {
     bgStyle() {
       return `background-image:url(${this.bgImage})`;
     }
   },
   mounted() {
+    this.imageHeight = this.$refs.bgImage.clientHeight;
+    this.minTranslateY = -this.imageHeight + RESERVED_HEIGHT;
     this.$refs.list.$el.style.top = `${this.$refs.bgImage.clientHeight}px`;
   },
-  data() {
-    return {};
+  created() {
+    this.probeType = 3;
+    this.listenScroll = true;
   },
-  methods: {}
+  data() {
+    return {
+      scrollY: 0
+    };
+  },
+  methods: {
+    scroll(pos) {
+      this.scrollY = pos.y;
+    },
+    back() {
+      this.$router.back();
+    }
+  },
+  watch: {
+    scrollY(newY) {
+      let translateY = Math.max(this.minTranslateY, newY);
+      let zIndex = 0;
+      let scale = 1;
+      let blur = 0;
+      this.$refs.layer.style[transform] = `translate3d(0,${translateY}px,0)`;
+      const percent = Math.abs(newY / this.imageHeight);
+      if (newY > 0) {
+        scale = 1 + percent;
+        zIndex = 10;
+      } else {
+        blur = Math.min(20 * percent, 20);
+      }
+      this.$refs.filter.style[backdrop] = `blur(${blur})px`;
+      if (newY < this.minTranslateY) {
+        zIndex = 10;
+        this.$refs.bgImage.style.paddingTop = 0;
+        this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`;
+        this.$refs.playBtn.style.display = 'none';
+      } else {
+        this.$refs.bgImage.style.paddingTop = '70%';
+        this.$refs.bgImage.style.height = 0;
+        this.$refs.playBtn.style.display = '';
+      }
+      this.$refs.bgImage.style.zIndex = zIndex;
+      this.$refs.bgImage.style[transform] = `scale(${scale})`;
+    }
+  }
 };
 </script>
 <style scoped lang="stylus">
@@ -98,7 +159,6 @@ export default {
     padding-top: 70%;
     transform-origin: top;
     background-size: cover;
-    z-index: 10;
 
     .play-wrapper {
       position: absolute;
